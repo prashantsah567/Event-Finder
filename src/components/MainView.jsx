fe4ae838da42a3ab2c7b92c2872d462775417b53
Prashant Sah <prashantsah567@gmail.com>
2023-03-26 15:47:09 -0400

    filter section added

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -6,7 +6,7 @@ const MainView = () =>{
     const [eventData, setEventData] = useState([]);
 
     async function fetchData() {
-        const response = await fetch("https://api.seatgeek.com/2/events?client_id=MzI2MTYwNzR8MTY3OTY5OTg2MS45ODAwMDAz")
+        const response = await fetch("https://api.seatgeek.com/2/events?per_page=100&client_id=MzI2MTYwNzR8MTY3OTY5OTg2MS45ODAwMDAz")
         const data = await response.json()
         console.log(data);
         setEventData(data.events);
@@ -19,6 +19,16 @@ const MainView = () =>{
   return (
     <div>
       <h1 className='header'>Explore Events Around You</h1>
+      <hr/>
+        <div className='search'>
+            <input placeholder='search by name of event'/> <button>Search</button>
+        </div>
+        <div className='filter'>
+            <p>First filter - State</p> 
+            <p>Second filter - Date</p>
+            <p>Third filter - Capacity</p>
+        </div>
+      <hr/>
       <div className='container'>
             {eventData.map(event => (
                 <div className='card'>
@@ -26,14 +36,14 @@ const MainView = () =>{
                         <strong>{event.title}</strong>
                     </p>
                     <p>
-                        Event Type: <span>{event.type}</span>
-                        Start Time (UTC): <span>{event.datetime_utc}</span>
-                        End Time (UTC): <span>{event.enddatetime_utc}</span>
+                        Event Type: <span>{event.type},</span>
+                        Start Time (UTC): <span>{event.datetime_utc},</span>
+                        End Time (UTC): <span>{(event.enddatetime_utc)?(event.enddatetime_utc):'N/A'}</span>
                     </p>
                     <p>
-                        <span>{event.venue.address}</span>
-                        <span>{event.venue.city}</span>
-                        <span>{event.venue.country}</span>
+                        <span>{event.venue.address},</span>
+                        <span>{event.venue.city},</span>
+                        <span>{event.venue.country},</span>
                         Capacity:<span>{(event.venue.capacity) > 0 ? (event.venue.capacity):'N/A'}</span>
                     </p>
                 </div>
