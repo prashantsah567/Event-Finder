9ce03da140ab1a0693cd0a72a3818cf4132d8b42
Prashant Sah <prashantsah567@gmail.com>
2023-03-26 13:27:54 -0400

    api fetch fixed

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -3,41 +3,29 @@ import './Style.css';
 
 const MainView = () =>{
 
-    const [data, setData] = useState([]);
+    const [titles, setTitles] = useState([]);
 
-    const url = `https://api.seatgeek.com/2/events?client_id=MzI2MTYwNzR8MTY3OTY5OTg2MS45ODAwMDAz`;
+    async function fetchData() {
+        const response = await fetch("https://api.seatgeek.com/2/events?client_id=MzI2MTYwNzR8MTY3OTY5OTg2MS45ODAwMDAz")
+        const data = await response.json()
+        const titleList = data.events.map(event => event.title);
+        setTitles(titleList);
+    }
+
     useEffect(() => {
-        async function fetchData() {
-            const response = await fetch(`${url}`);
-            const jsonData = await response.json();
-            setData(jsonData);
-        }
         fetchData();
     }, []);
-    
-    return(
-        <div>
-            <div className='container'>
-                <p>Data from the API call</p>
-                <p></p>
-                {/* {
-                    data.map(event => (
-                        <div key={event.id}>
-                            <p>{event.events[1].title}</p>
-                        </div>
-                    ))
-                } */}
-            
-                {
-                    <p>{data.events[0].title}</p>
-                }
-                
-                {
-                    console.log(data)
-                }
-            </div>
-        </div>
-    );
+
+  return (
+    <div>
+      <h1>List of Titles:</h1>
+      <ul>
+        {titles.map(title => (
+          <li key={title}>{title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default MainView;
