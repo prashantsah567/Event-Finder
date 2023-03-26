7cca1d07321ee9c4295ee7df1f3b217609534484
Prashant Sah <prashantsah567@gmail.com>
2023-03-26 14:43:16 -0400

    data and background image set

--- src/components/MainView.jsx
+++ src/components/MainView.jsx
@@ -3,13 +3,13 @@ import './Style.css';
 
 const MainView = () =>{
 
-    const [titles, setTitles] = useState([]);
+    const [eventData, setEventData] = useState([]);
 
     async function fetchData() {
         const response = await fetch("https://api.seatgeek.com/2/events?client_id=MzI2MTYwNzR8MTY3OTY5OTg2MS45ODAwMDAz")
         const data = await response.json()
-        const titleList = data.events.map(event => event.title);
-        setTitles(titleList);
+        console.log(data);
+        setEventData(data.events);
     }
 
     useEffect(() => {
@@ -18,12 +18,27 @@ const MainView = () =>{
 
   return (
     <div>
-      <h1>List of Titles:</h1>
-      <ul>
-        {titles.map(title => (
-          <li key={title}>{title}</li>
-        ))}
-      </ul>
+      <h1 className='header'>Explore Events Around You</h1>
+      <div className='container'>
+            {eventData.map(event => (
+                <div className='card'>
+                    <p key={event.id}>
+                        <strong>{event.title}</strong>
+                    </p>
+                    <p>
+                        Event Type: <span>{event.type}</span>
+                        Start Time (UTC): <span>{event.datetime_utc}</span>
+                        End Time (UTC): <span>{event.enddatetime_utc}</span>
+                    </p>
+                    <p>
+                        <span>{event.venue.address}</span>
+                        <span>{event.venue.city}</span>
+                        <span>{event.venue.country}</span>
+                        Capacity:<span>{(event.venue.capacity) > 0 ? (event.venue.capacity):'N/A'}</span>
+                    </p>
+                </div>
+            ))}
+      </div>
     </div>
   );
 }
